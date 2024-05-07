dcc75ec957cc66b70febceadbd1250e034b6d067
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-07 16:22:53 +0300

    07.05 - begin - 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native'
+import {useState} from 'react'
+
+type Tasks = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [textInputValue, setTextInputValue] = useState('TextInput')
+    const [tasks, setTasks] = useState<Tasks[]>([
+        {
+            id: 1,
+            title: 'HTML',
+            isDone: true
+        },
+        {
+            id: 2,
+            title: 'React',
+            isDone: false
+        },
+        {
+            id: 3,
+            title: 'CSS',
+            isDone: true
+        }
+    ])
+
+
+    return (
+        <View style={styles.container}>
+            <TextInput value={textInputValue}
+                       onChangeText={setTextInputValue}
+                       style={styles.textInput}/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#355434',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textInput: {
+        width: 300,
+        backgroundColor: '#c9be1c',
+        color: '#fff',
+        fontSize: 25,
+        borderRadius: 5,
+        padding: 10,
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderColor: 'blue'
+    }
+})
+
+// const globalStyles = StyleSheet.create({
+//     border: {
+//         borderStyle: 'solid',
+//         borderWidth: 2,
+//         borderColor: 'blue'
+//     }
+// })
